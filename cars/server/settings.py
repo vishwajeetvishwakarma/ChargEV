21bc239c0e3f01f2d9583f14f0bec903fdb08a4a
vishwajeetvishwakarma <112095008+vishwajeetvishwakarma@users.noreply.github.com>
2023-02-13 20:00:43 +0530

    Update settings.py

--- cars/server/settings.py
+++ cars/server/settings.py
@@ -173,5 +173,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = '20102144.vishwajeet.vishwakarma@gmail.com'
-EMAIL_HOST_PASSWORD = 'gvhoqepjzodsjmsp'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
